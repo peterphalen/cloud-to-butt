734a2c56e1b35e6e8b53a75482c77a1539bbcb75
Peter Phalen <PeterPhalen@Peters-MacBook-Air-2.local>
2016-03-13 15:46:05 -0400

    tolerance for pluralization

--- Source/content_script.js
+++ Source/content_script.js
@@ -36,10 +36,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
     
-    v = v.replace(/\bIllegal Alien\b/g, "Human being (here referred to as 'Illegal Alien')");
-	v = v.replace(/\bIllegal alien\b/g, "Human being (here referred to as 'Illegal alien')");
-	v = v.replace(/\billegal Alien\b/g, "Human being (here referred to as 'illegal Alien')");
-	v = v.replace(/\billegal alien\b/g, "Human being (here referred to as 'illegal alien')");
+    v = v.replace(/\bIllegal Alien(s)\b/g, "Human being$ (here referred to as 'Illegal Alien$')");
+	v = v.replace(/\bIllegal alien(s)\b/g, "Human being$ (here referred to as 'Illegal alien$')");
+	v = v.replace(/\billegal Alien(s)\b/g, "Human being$ (here referred to as 'illegal Alien$')");
+	v = v.replace(/\billegal alien(s)\b/g, "Human being$ (here referred to as 'illegal alien$')");
 	
 	textNode.nodeValue = v;
 }
